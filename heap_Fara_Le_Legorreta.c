84c65f0e3495f740ed5dc9591a4f821a46d145bd
computer-science-student <legorrej@oregonstate.edu>
2021-07-31 21:04:00 -0500

    Added the functions

---
+++ heap_Fara_Le_Legorreta.c
@@ -0,0 +1,10 @@
+// heapify array A containing n elements
+void heapify(int A[], int n);
+
+
+// heapsort array A containing n elements
+void heapsort(int A[], int n);
+
+
+// percolate the key at index k down the heap A containing n elements
+void percolateDown(int A[], int n, int index);
