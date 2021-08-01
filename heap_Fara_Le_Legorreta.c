6b5b385e35dd8b000d42c3f024d881563fa69611
computer-science-student <legorrej@oregonstate.edu>
2021-07-31 21:51:44 -0500

    I have written comments as notes to myself

--- heap_Fara_Le_Legorreta.c
+++ heap_Fara_Le_Legorreta.c
@@ -3,14 +3,24 @@
 #include "heap.h"
 
 // heapify array A containing n elements
-void heapify(int A[], int n);
+void heapify(int A[], int n){
+    // will have to do a breath first traversal from the bottom to top.
 
+}
 
 // heapsort array A containing n elements
 void heapsort(int A[], int n){
+    // In decending order. In order to have the array be in decending order
+    //one will need to make use of the max heap.
+    // heapsort will call heapify.
+    // This sorts the array.
     return;
 }
 
-
 // percolate the key at index k down the heap A containing n elements
-void percolateDown(int A[], int n, int index);
+void percolateDown(int A[], int n, int index){
+    // start here
+    //
+
+
+}
